4399e0ad3a2cb0b9b3c7ccfa433cbb3202465900
Joseph Tawil <jtawil2015@gmail.com>
2020-06-11 04:38:35 -0700

    added post and put

--- routes/data-route.js
+++ routes/data-route.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 router.post("/api", (req, res) => {
     // res.json({ message: "success" });
+    console.log(req.body);
     res.json(req.body);
+});
+
+router.post("/api/test/:fname/:lname", (req, res) => {
+    console.log(req.params);
+    res.json(req.params);
+});
+
+//used more in industry
+router.put("/api/query", (req, res) => {
+    console.log(req.query);
+    res.json(req.query);
 });
 module.exports = router;
